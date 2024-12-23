288dd7cc5e8dbf9965ce13874d5a33c52a25b955
Karthik A V PHP Developer <karthikav1999@gmail.com>
2024-12-23 20:50:54 +0530

    fix

--- src/Arrays/Secondlarg.java
+++ src/Arrays/Secondlarg.java
@@ -1,6 +1,6 @@
 public class Secondlarg {
     public static void main(String[] args) {
-        int a[] = {10,5,18,20,15};
+        int a[] = {100,5,18,20,15};
         int n = a.length;
 
         int res = sec(a,n);
@@ -10,47 +10,46 @@ public class Secondlarg {
     private static int sec(int[] a, int n) {
         
         // naive
-        // int sec = -1;
-        // int l = 0;
-        // for (int i = 0; i < n; i++) {
-        //     if(a[i] > a[l]){
-        //         l = i;
-        //     }
+        int sec = -1;
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            if(a[i] > a[l]){
+                l = i;
+            }
           
-        // }
+        }
 
-        // for (int i = 0; i < n; i++) {
-        //     if(a[i] != a[l]){
-        //         if(sec == -1){
-        //             sec =0;
-        //         }else if(a[i]>a[sec]){
-        //             sec=i;
-        //         }
-        //     }
-        // }
+        for (int i = 0; i < n; i++) {
+            if(a[i] != a[l]){
+                
+                if(sec==-1 || a[i]>a[sec]){
+                    sec=i;
+                }
+            }
+        }
         
-        // return sec;
+        return sec;
 
         // naive
 
         // better
 
-        int sec = -1;
-        int l =0;
+        // int sec = -1;
+        // int l =0;
 
-        for (int i = 0; i < n; i++) {
-            if(a[i] > a[l]){
+        // for (int i = 0; i < n; i++) {
+        //     if(a[i] > a[l]){
                 
-                sec =l;
-                l = i;
-            }else if(a[i] != a[l]){
-                if(sec==-1 || a[i] >a[sec]){
-                    sec = i;
-                }
-            }
-        }
+        //         sec =l;
+        //         l = i;
+        //     }else if(a[i] != a[l]){
+        //         if(sec==-1 || a[i] >a[sec]){
+        //             sec = i;
+        //         }
+        //     }
+        // }
 
-        return sec;
+        // return sec;
 
     }
 }
